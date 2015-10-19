5e3703b1507b04dd3c31a4c7f3581ded19ae4638
Sebastiaan Mathot <s.mathot@cogsci.nl>
2015-10-19 11:34:19 +0200

    Bump to 0.6.0a2

--- pygaze/__init__.py
+++ pygaze/__init__.py
@@ -19,7 +19,7 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-version = u'0.6.0a1'
+version = u'0.6.0a2'
 
 from pygaze.settings import settings
 
